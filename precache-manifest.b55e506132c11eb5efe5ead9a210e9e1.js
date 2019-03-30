bb5c338a8cc265ef8e133b2ecd06d3f3f77e8369
Fehér Gergő <feher.gergo9@gmail.com>
2019-03-30 21:50:24 +0100

    Updates

---
+++ precache-manifest.b55e506132c11eb5efe5ead9a210e9e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ac4f881a05dc683cb92",
+    "url": "/static/js/main.95beb93f.chunk.js"
+  },
+  {
+    "revision": "294689b2c19dce27b627",
+    "url": "/static/js/2.8cb0cd55.chunk.js"
+  },
+  {
+    "revision": "0ac4f881a05dc683cb92",
+    "url": "/static/css/main.92a2148b.chunk.css"
+  },
+  {
+    "revision": "3b8bd78b377a1de40d0d6317ff100350",
+    "url": "/index.html"
+  }
+];
